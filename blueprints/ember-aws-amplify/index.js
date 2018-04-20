dbf9af88d3ac1deb7a97f2bc50075db353aa6ce4
Graham Towse <grimbonious@hey.computer>
2018-04-21 09:21:36 +1000

    Add default blueprint

---
+++ blueprints/ember-aws-amplify/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  afterInstall() {
+    // Add addons to package.json and run defaultBlueprint
+    return this.addAddonsToProject({
+      // a packages array defines the addons to install
+      packages: [
+        // name is the addon name, and target (optional) is the version
+        {
+          name: 'ember-cli-webpack-imports'
+        },
+      ]
+    }).then(() => {
+      // Add npm packages to package.json
+      return this.addPackagesToProject([
+        {name: 'aws-amplify'}
+      ]);
+    })
+  }
+};
